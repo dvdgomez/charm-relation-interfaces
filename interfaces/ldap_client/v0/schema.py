7ecf1e2f662ddd0ec235592dadd1ad0a48b2c202
David Gomez <david.gomez@canonical.com>
2023-04-13 10:15:33 -0700

    add provider json doc + schema update

--- interfaces/ldap_client/v0/schema.py
+++ interfaces/ldap_client/v0/schema.py
@@ -1,7 +1,18 @@
-"""This file defines the schemas for the provider and requirer sides of this relation interface.
+"""This file defines the schemas for the provider and requirer sides of the ldap-client interface.
 
 It must expose one interfaces.schema_base.DataBagSchema subclasses called:
 - ProviderSchema
+
+Examples:
+    ProviderSchema:
+        unit: <empty>
+        app: {
+            "basedn": "dc=glauth,dc=com"
+            "ca_cert": "glauth.crt contents"
+            "ldap_default_bind_dn": "cn=serviceuser,ou=svcaccts,dc=glauth,dc=com"
+            "ldap_password": "mysecret"
+            "ldap_uri": "ldaps://jujuabc-123:3894"
+        }
 """
 
 from pydantic import BaseModel
@@ -9,14 +20,14 @@ from pydantic import BaseModel
 from interfaces.schema_base import DataBagSchema
 
 
-class MyProviderAppData(BaseModel):
-    basedn: str
-    ca_cert: str
-    ldap_default_bind_dn: str
-    ldap_password: str
-    ldap_uri: str
+class LdapClientProviderAppData(BaseModel):
+    basedn: str # base dn
+    ca_cert: str # CA certificate contents.
+    ldap_default_bind_dn: str # default bind dn.
+    ldap_password: str # ldap password.
+    ldap_uri: str # LDAP uri. 
 
 
 class ProviderSchema(DataBagSchema):
-    """The schema for the provider side of this interface."""
-    app: MyProviderAppData
+    """Provider Schema for ldap-client."""
+    app: LdapClientProviderAppData
